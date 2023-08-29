0f635395a4944be2352671ea9c50214094fc63cf
ticoniq_ <okolietochi@gmail>
2023-08-29 04:43:22 +0100

    add Person, Student and Teacher classes

---
+++ classes/person.rb
@@ -0,0 +1,25 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age = nil, name = 'Unknown', parent_permission: true)
+    @id = generate_id
+    @name = name
+    @parent_permission = parent_permission
+    @age = age
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age && @age >= 18
+  end
+
+  def generate_id
+    rand(1..1000)
+  end
+end
