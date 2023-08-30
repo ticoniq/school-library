86dcdca2e8cc418a7dc09a6531cc11c962f92c7c
ticoniq_ <okolietochi@gmail>
2023-08-30 01:26:52 +0100

    OOP school library: decorate a class

--- classes/person.rb
+++ classes/person.rb
@@ -1,5 +1,10 @@
-class Person
+require './nameable'
+require './capitilize_decorator'
+require './trimmer_decorator'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -12,9 +17,20 @@ class Person
     of_age? || @parent_permission
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age && @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
